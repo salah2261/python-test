acfc809a4085a4a8b5bd336e7d92aef61952cb7d
salah2261 <i@slahuddin.com>
2021-07-17 10:28:26 +0300

    I added requests api for github

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import datetime
+import requests, json
 
 if __name__ == '__main__':
-	print(datetime.datetime.now())
+	# print(datetime.datetime.now())
+	url = "https://api.github.com/users/%(username)s" % dict(username="salah2261")
+	res = requests.get(url)
+
+	loadeddata = json.loads(res.text)
+
+	print(json.dumps(loadeddata, indent=4, sort_keys=True))
